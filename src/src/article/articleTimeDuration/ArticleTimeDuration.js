24c3a7bb157f76f6ef3b648a06249eb216f51934
Slin castro <scastro@thoughtworks.com>
2017-12-26 16:49:10 -0300

    #16 con @alfredosegundo transform ArticleDurationTime component to stateless

--- src/src/article/articleTimeDuration/ArticleTimeDuration.js
+++ src/src/article/articleTimeDuration/ArticleTimeDuration.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ArticleTimeDuration.css';
 
-class ArticleTimeDuration extends Component {
-    render() {
+const ArticleTimeDuration = () =>
+  {
         return (
             <div>
                 <aside>Tiempo de lectura: 4 minutos</aside>
             </div>
         );
     }
-}
 
 export default ArticleTimeDuration;
