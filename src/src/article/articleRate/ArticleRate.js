d5a413a9e49d6cfaf1b3fea5f323c254c0e3c3a2
Slin castro <scastro@thoughtworks.com>
2018-01-04 12:15:17 -0300

    [#14] set article width

---
+++ src/src/article/articleRate/ArticleRate.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ArticleRate.css';
+
+const ArticleRate = () => {
+        return (
+            <div className="Article-rate"></div>
+        );
+};
+
+export default ArticleRate;
---
+++ src/src/article/articleRate/ArticleRate.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ArticleRate from './ArticleRate';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const wrapper = shallow(<ArticleRate />)
+
+it('render component', () => {
+    expect(wrapper).toBeDefined()
+})
+
+it('contains a rate div', () => {
+    expect(wrapper.find('div.Article-rate').exists()).toBeTruthy()
+})
