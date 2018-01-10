e3e77504be7b4edf7523e473c8251efaf168f12a
Slin castro <scastro@thoughtworks.com>
2018-01-09 23:49:43 -0300

    [#22] add hoover, active style in face outline

--- src/src/article/articleRate/smileFace/SmileFace.js
+++ src/src/article/articleRate/smileFace/SmileFace.js
@@ -8,7 +8,7 @@ const SmileFace = () => {
                 <g >
                     <circle className="smile-eye" r='7' cx='36' cy='39' />
                     <circle className="smile-eye" r='7' cx='70' cy='39' />
-                    <circle className="smile-right-eyelid" r='15' id='c' cx='75' cy='3' />
+                    <circle className="smile-right-eyelid" r='15' id='c' cx='70' cy='3' />
                     <circle className="smile-face-outline" r='45' cx='53' cy='53'/>
                     <g className='smile-mouth' transform='translate(23,67)'>
                         <path d="M0,0 A40,40 10 0,0 60,0" />	
