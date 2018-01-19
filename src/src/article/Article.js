29bd12bd44ceec60d6f2c8997cb1a10b946b5d9e
p3cycle <crivera@thoughtworks.com>
2018-01-19 12:15:03 -0300

    [Cristina] Add missing sections on feminism article content

--- src/src/article/Article.js
+++ src/src/article/Article.js
@@ -37,6 +37,30 @@ Una de las discusiones alrededor del movimiento es:  si el feminismo busca la ig
              <p>
              Sin embargo, a pesar de esta percibida igualdad, aún existe discriminación y estamos lejos de reflejar una presencia igualitaria en diferentes instancias de la sociedad, más aún, la valoración de las mujeres por su físico y no por su capacidad, sigue siendo el común incluso en las sociedades occidentales <a href="http://www.imdb.com/title/tt1784538/?ref_=tt_rec_tt">[ver Miss Representation]</a> y, aunado a esto el problema latente de la violencia contra mujeres y niñas, en sus diferentes formas.
             </p>
+            <h3>Datos y debates en torno al feminismo en Chile:</h3>
+            <ul>
+                <li> <a href="http://www.memoriachilena.cl/602/w3-article-95954.html">Precursoras del Feminismo en Chile</a></li>
+                <li> <a href="http://www.minmujeryeg.cl/femicidios/que-es/">Femicidios en Chile al 2017</a></li>
+                <li> <a href="http://www.quepasa.cl/articulo/actualidad/2017/06/no-hay-refugio.shtml/">Violencia de Género en Chile. No hay refugio.</a></li>
+                <li> <a href="http://www.elmostrador.cl/braga/2017/02/14/ley-de-violencia-intrafamiliar-las-razones-de-por-que-se-dejo-fuera-el-pololeo/">Violencia en el pololeo. Un tema pendiente.</a></li>
+                <li> <a href="http://www.bcn.cl/observatorio/americas/noticias/leyes-integrales-de-violencia-contra-las-mujeres-en-america-latina-el-caso-de-uruguay">Leyes integrales de violencia contra las mujeres en América Latina: el caso de Uruguay</a></li>
+                <li> <a href="https://www.leychile.cl/Consulta/listado_n_sel?_grupo_aporte=&sub=828&agr=2&comp=">Mujer, normas desde 1990</a></li>
+            </ul>
+            <h3>Datos y debates en torno al feminismo en Ecuador:</h3>
+            <ul>
+                <li> <a href="http://www.bbc.com/mundo/noticias-america-latina-37828573">Mapa de femicidios por país</a></li>
+                <li> <a href="http://www.eluniverso.com/noticias/2017/01/24/nota/6014020/casos-femicidos-ecuador-se-han-duplicado-2017-segun-dinased">Casos de femicidio en Ecuador</a></li>
+                <li> <a href=" http://www.andes.info.ec/es/noticias/mujeres-luchan-equidad-laboral-ecuador.html">Equidad laboral</a></li>
+                <li>El acoso sigue siendo pan de cada día en la sociedad ecuatoriana: #PrimerAcoso #NoCallamosMás - Grupo secreto en Facebook que tiene más de 27.000 miembros</li>
+                <li> <a href="http://elecciones2017.gkillcity.com/2017/02/03/leyes-machistas-en-ecuador/">Un link interesante acerca de las leyes machistas que han existido éstos últimos años en Ecuador</a></li>
+            </ul>
+            <h3>El rol de los hombres en la lucha</h3>
+            <p>
+                Uno de los debates dentro de esta lucha, fue entender qué papel ocupan los hombres en la liberación de las mujeres y la lucha contra el sistema patriarcal en general. En ese sentido han existido debates muy interesantes acerca de si <a href="http://www.tribunafeminista.org/2017/01/el-papel-de-los-hombres-en-la-lucha-feminista/">el rol de hombre rol en la lucha feminista es el de acompañamiento, solidaridad, apoyo y complicidad </a> o si en realidad <a href="https://horizontal.mx/hombres-feministas-y-el-problema-del-privilegio-masculino/">el hombre si puede ser considerado como feminista (a pesar de que la sociedad le atribuya privilegios).</a>
+            </p>
+            <p>
+                <em>[“Debemos evitar apropiarnos de la lucha relativa a las cuestiones de género. Siempre debemos ser conscientes de ello. La tentación existe debido a la tendencia dominante de los hombres. Debemos mantenernos vigilantes y recordar que ésta es una lucha que tienen que liderar las mujeres”. Mbuyiselo Botha, Foro de hombres sudafricanos]</em>
+            </p>
         </section>
         <ArticleRate/>
       </div>
